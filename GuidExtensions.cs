bb7e75117bafffab06fe13b003bb45e75ee74169
ParamVarunReddy <82122463+ParamVarunReddy@users.noreply.github.com>
2023-01-05 14:29:50 -0800

    Guid Extensions and

---
+++ GuidExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace PCPPlugins.BusinessLayer.Extensions
+{
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="GuidExtensions" />.
+    /// </summary>
+    public static class GuidExtensions
+    {
+        public static bool IsNullOrEmpty(this Guid? guid)
+        {
+            return !guid.HasValue || guid.Value.Equals(Guid.Empty);
+        }
+
+    }
+}
---
+++ JsonWebEncryptionExtension.cs
@@ -0,0 +1,55 @@
+﻿namespace PCPPlugins.BusinessLayer.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using Microsoft.IdentityModel.Tokens;
+    using Microsoft.IdentityModel.JsonWebTokens;
+    
+
+    public class JsonWebEncryptionExtension
+    {
+        private string PublicKeySign { get; set; }
+        private string PrivateKeyEncrypt { get; set; }
+
+        public IDictionary<string, object> Claims { get; }
+        public ClaimsIdentity ClaimsIdentity { get; }
+        public Exception Exception { get; }
+        public string Issuer { get; }
+        public bool IsValid { get; }
+        public IDictionary<string, object> PropertyBag { get; }
+        public SecurityToken SecurityToken { get; }
+        public CallContext TokenContext { get; }
+        public string TokenType { get; }
+
+        public JsonWebEncryptionExtension(string _publicKeySign, string _privateKeyEncrypt)
+        {
+            PublicKeySign = _publicKeySign;
+            PrivateKeyEncrypt = _privateKeyEncrypt;
+        }
+
+        private static TokenValidationResult DecryptAndValidateJwe(string token, SecurityKey signingKey, SecurityKey encryptionKey)
+        {
+            var handler = new JsonWebTokenHandler();
+
+            TokenValidationResult result = handler.ValidateToken(
+                token,
+                new TokenValidationParameters
+                {
+                    ValidAudience = "api1",
+                    ValidIssuer = "https://idp.example.com",
+
+                    // public key for signing
+                    IssuerSigningKey = signingKey,
+
+                    // private key for encryption
+                    TokenDecryptionKey = encryptionKey
+                });
+
+            return result;
+        }
+    }
+}
+
+//https://stackoverflow.com/questions/49328317/how-to-decrypt-jwe-source-encrypted-with-rsa1-5-a256cbc-hs512-in-c
+//https://www.scottbrady91.com/c-sharp/json-web-encryption-jwe-in-dotnet-core
